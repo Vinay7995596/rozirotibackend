7baa6c3018971588ec82ba81ff66aae4750c8a9b
Vinay7995596 <124029698+Vinay7995596@users.noreply.github.com>
2024-02-12 14:35:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
+const port = 'https://backend-gd98.onrender.com/' || 3001;
 
 app.use(express.json());
 app.use(cors()); // Enable CORS
@@ -77,5 +77,4 @@ app.post('/transaction', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
 });
